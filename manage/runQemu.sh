2a743e64a834ddf97eac8d3ce49e20a2859111e3
artsemenka <artsemenka@gmail.com>
2013-08-14 19:38:02 +0300

    2

--- manage/runQemu.sh
+++ manage/runQemu.sh
@@ -15,6 +15,29 @@
 #   limitations under the License.
 #
 
-sudo ../qemu/x86_64-softmmu/qemu-system-x86_64 -m 1024 -hda ./vdisk/10GB.img -device nvme,namespaces=2,size=512 -smp 2 -smb /home/abc/install 
+vdisk=$1
+isofile=$2
+
+Usage() {
+echo "usage...."
+echo "  $0 <HD_img> [OS_ISO]"
+echo "    <HD_img>  Pass the name of your virtual HD within the ./vdisk dir"
+echo "    [OS_ISO]  Optional param, use only when attempting to install a"
+echo "              virtual OS onto a new ./vdisk image. Pass ISO filename."
+echo ""
+}
+
+if [ -z $vdisk ]; then
+	Usage
+	exit
+fi
+
+if [ -z $isofile ]; then
+	# sudo needed because kvm module requires higher priveledged user; default
+    # 4 namespaces each of size 512MB.
+	sudo ../qemu/x86_64-softmmu/qemu-system-x86_64 -m 4096 -hda ./vdisk/${vdisk} -device nvme,namespaces=2,size=512 --enable-kvm -smp 2
+else
+	../qemu/x86_64-softmmu/qemu-system-x86_64 -m 4096 -cdrom ./vkernel/${isofile} -hda ./vdisk/${vdisk} -device nvme
+fi
 
 exit
